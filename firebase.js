729cd16759e71ca5d3d24498070215e034289fcf
Luis Noguera Arbos <luisnoguera@Luiss-MacBook-Pro-2.local>
2024-05-20 13:50:23 +0200

    Update HTML files and Firebase integration

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDgUAojmCBprTeY7_o3xGnDhDuPxOPvD_g",
+  authDomain: "portfolio-3014b.firebaseapp.com",
+  projectId: "portfolio-3014b",
+  storageBucket: "portfolio-3014b.appspot.com",
+  messagingSenderId: "764292816862",
+  appId: "1:764292816862:web:4e6fa598a3fc7b774ec74f",
+  measurementId: "G-F9J431P1SW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth, signInWithEmailAndPassword };
